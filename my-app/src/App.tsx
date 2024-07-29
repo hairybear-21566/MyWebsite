877117fd7e9f88f296792510db52b6fbba44c1d4
hairybear-21566 <hairbear21566@gmail.com>
2024-07-29 12:47:15 +0400

    navbar working

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,12 +5,13 @@ import ReactDOM from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
 
 import {Projects, Landing, Blog, Contact, About} from './Components/Pages/index';
-
+import {Navbar} from "./Components/Pages/Parts/index";
 
 function App() {
   //<Route path="*" element={<Projects />} />
   return (
-
+<>
+<Navbar></Navbar>
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/projects" element={<Projects />}/>
@@ -18,7 +19,7 @@ function App() {
       <Route path="/contactme" element={<Contact />} />
       <Route path="/aboutme" element={<About />} />
     </Routes>
-
+    </>
   );
 }
 
